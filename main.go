22e13ced199820e712f212ed22ae729941cf93b5
lianhong <2758988938@qq.com>
2023-06-07 13:31:21 +0800

    优化配置流程

--- main.go
+++ main.go
@@ -19,11 +19,18 @@ import (
 func init() {
 	f, err := os.ReadFile("config.json")
 	if err != nil {
-		panic(err)
+		f, err := os.Create("config.json")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer f.Close()
+		configdata, _ := json.MarshalIndent(zero.MYSconfig, "", "  ")
+		_, _ = f.Write(configdata)
+		log.Fatalln("创建初始化配置完成\n请填写config.json文件后重新运行本程序\n字段解释:\ntoken:机器人基本信息,eventpath:回调路径,port:端口\nhost:你的服务器地址/外部访问地址,如果不是80端口,需要加上端口号,结尾不需要加\"/\"")
 	}
 	err = json.Unmarshal(f, &zero.MYSconfig)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	if zero.MYSconfig.BotToken.BotID == "" || zero.MYSconfig.BotToken.BotSecret == "" {
 		log.Fatalln("[init]未设置bot信息")
