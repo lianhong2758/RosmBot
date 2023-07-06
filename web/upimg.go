135d485eb94e3e8c0097aad8cf06c18c0a63837f
lianhong <2758988938@qq.com>
2023-07-06 14:23:12 +0800

    fix https

--- web/upimg.go
+++ web/upimg.go
@@ -13,8 +13,8 @@ import (
 )
 
 const (
-	fileUrl = "http://bg.tencentbot.top/image/uploadfile/"
-	imgUrl  = "http://bg.tencentbot.top/image/upload?url="
+	fileUrl = "https://bg.tencentbot.top/image/uploadfile/"
+	imgUrl  = "https://bg.tencentbot.top/image/upload?url="
 )
 
 // 上传byte数据
