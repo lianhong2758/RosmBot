5b86f4e58b7402f9e1151d931aed02ae852eb647
lianhong <2758988938@qq.com>
2023-08-13 14:49:32 +0800

    fix

--- plugins/chatgpt/chatgpt.go
+++ plugins/chatgpt/chatgpt.go
@@ -14,20 +14,7 @@ const (
 	// baseURL  = "https://api.openai.com/v1/"
 	proxyURL           = "https://open.aiproxy.xyz/v1/"
 	modelGPT3Dot5Turbo = "gpt-3.5-turbo"
-	yunKey             = "7d06a110e9e20a684e02934549db1d3d"
-	yunURL             = "https://api.a20safe.com/api.php?api=35&key=%s&apikey=%s"
 )
-
-type yun struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data []struct {
-		Return    string `json:"return"`
-		Total     string `json:"total"`
-		Available string `json:"available"`
-		Used      string `json:"used"`
-	} `json:"data"`
-}
 
 // chatGPTResponseBody 响应体
 type chatGPTResponseBody struct {
